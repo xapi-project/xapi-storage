5d7c0e9ecc1f44614a2a4aab54daf2d3f03e6823
Jon Ludlam <jonathan.ludlam@citrix.com>
2018-04-30 15:09:50 +0100

    Use cmdliner for the code generator
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- generator/src/main.ml
+++ generator/src/main.ml
@@ -1,13 +1,32 @@
+open Cmdliner
 
-let _ =
+let gen_python path =
   let open Types in
   let open Files in
 
   List.iter
     (fun api ->
-       with_output_file (Printf.sprintf "./xapi/storage/api/v4/%s.py" api.Codegen.Interfaces.name)
+       with_output_file (Printf.sprintf "%s/%s.py" path api.Codegen.Interfaces.name)
          (fun oc ->
             let p = Pythongen.of_interfaces api |> Pythongen.string_of_ts in
             output_string oc p
          )
-    ) Apis.apis
+    ) Apis.apis;
+  `Ok ()
+
+let gen_python_cmd =
+  let doc = "Generate the python library files" in
+  let path =
+    let doc = "Generate the files in the path specified" in
+    Arg.(value & opt string ("./xapi/storage/api/v4") & info ["p";"path"] ~docv:"PATH")
+  in
+  Term.(ret (const gen_python $ path)),
+  Term.info "gen_python" ~doc ~exits:Term.default_exits
+
+let default_cmd =
+  let doc = "SMAPI code/documentation generation tool" in
+  Term.(ret (const (fun _ -> `Help (`Pager, None)) $ const ())),
+  Term.info "main" ~doc ~exits:Term.default_exits
+
+let _ =
+  Term.(exit @@ eval_choice default_cmd [gen_python_cmd])
